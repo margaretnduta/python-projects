54bad143a24c4683af9f7ea44a0c437f907df43c
Margaret Wambui <127622433+margaretnduta@users.noreply.github.com>
2026-04-01 20:40:13 +0300

    I have created my first ever rock paper scissor game...hahaha

---
+++ rock paper scissors.py
@@ -0,0 +1,10 @@
+
+import random
+def get_choices():
+     player_choice =input("Enter a choice (rock,paper,scissors: ")
+     options =['rock','paper','scissor']
+     computer_choice=random.choice(options)
+     choices={"player":player_choice,"computer":computer_choice}
+     return choices
+def check_win(Player,computer):
+    return check_win
